af3f4ed0cd4e9f7f514a2dfedfcf9403f84ff714
yahia <yahiaeltaralm@gmail.com>
2024-11-16 23:02:15 +0200

    adding computed fields to Meal model & action decorator to MealViewSet

--- api/views.py
+++ api/views.py
@@ -1,7 +1,10 @@
-from rest_framework import viewsets, filters
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, filters, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Meal, Rating
 from .serializers import MealSerializer, RatingSerializer
-
+from django.contrib.auth.models import User
 
 
 
@@ -10,8 +13,46 @@ from .serializers import MealSerializer, RatingSerializer
 class MealViewSet(viewsets.ModelViewSet):
     queryset = Meal.objects.all()
     serializer_class = MealSerializer
-    lookup_field = 'slug'
+    # lookup_field = 'slug'
 
+
+    @action(methods= ['POST'], detail= True)
+    def rate_meal(self, request, pk=None):
+        if 'stars' in request.data:
+            '''
+            create or update
+            '''
+            meal = get_object_or_404(Meal, id=pk)
+            username = request.data['username']
+            user = get_object_or_404(User, username=username)
+            stars = request.data['stars']
+
+            try:
+                # update
+                rating = Rating.objects.get(meal = meal.id, user=user.id)
+                rating.stars = stars
+                rating.save()
+                serializer_data = RatingSerializer (rating, many = False).data
+                json = {'message': 'Rating updated successfully',
+                        'result' : serializer_data
+                        }
+                return Response(json, status=status.HTTP_200_OK)
+
+            except:
+                # create if the rate is not exist
+                rating = Rating.objects.create(user= user, meal= meal, stars= stars)
+                serializer_data = RatingSerializer(rating, many = False).data
+                json = {'message': 'Rating created successfully',
+                        'result' : serializer_data
+                        }
+                return Response(json, status=status.HTTP_201_CREATED)
+
+
+        else:
+            json = {
+                'message': 'stars not provided'
+            }
+            return Response(json, status=status.HTTP_400_BAD_REQUEST)
 
 
 class RatingViewSet(viewsets.ModelViewSet):
